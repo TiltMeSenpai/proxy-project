965216a3c94abf20f709cd7a65e13cfcc0a82c5c
Josh Koike <jk@tilting.me>
2022-01-06 14:57:35 -0800

    Refactor Proxy to use tower::Service

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,3 @@
-use std::convert::Infallible;
-use std::io::Error;
-use std::net::SocketAddr;
-use std::sync::Arc;
-
-use hyper::http::uri::{Authority, Scheme};
-use hyper::server::conn::{AddrStream, Http};
-use hyper::service::{make_service_fn, service_fn};
-use hyper::upgrade::Upgraded;
-use hyper::{Body, Client, Method, Request, Response, Server, Uri};
-
-use rustls::ServerConfig;
-use tokio_rustls::TlsAcceptor;
-
 mod cert;
 mod proxy;
 
@@ -19,5 +5,5 @@ mod proxy;
 async fn main() {
     let config: proxy::ProxyConfig = Default::default();
     let proxy = config.build();
-    let _ = proxy::Proxy::run(proxy).await.unwrap();
+    let _ = proxy::ProxyServer::run(proxy).await.unwrap();
 }
--- src/proxy.rs
+++ src/proxy.rs
@@ -2,20 +2,22 @@
 
 use std::convert::Infallible;
 use std::future::Future;
-use std::io::Error;
+use std::io::{Error, ErrorKind};
 use std::net::SocketAddr;
-use std::path::Path;
+use std::pin::Pin;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicU32, Ordering};
+use std::task::Poll;
 
 use hyper::http::uri::{Authority, Scheme};
 use hyper::server::conn::{Http, AddrStream};
-use hyper::service::{service_fn, make_service_fn};
-use hyper::upgrade::Upgraded;
+use hyper::service::Service;
+use hyper::upgrade::{Upgraded, self};
 use hyper::{Request, Response, Body, Method, Uri, Client, Server};
+use rustls::ServerConfig;
 use tokio::task::JoinHandle;
 use tokio::sync::mpsc::{Sender,Receiver, channel};
-use tokio_rustls::TlsAcceptor;
+use tokio_rustls::{TlsAcceptor, server::TlsStream};
 use crate::cert::{CertStore};
 
 
@@ -35,8 +37,8 @@ struct ProxyEvent {
 pub struct ProxyConfig {
     pub pubkey_path: String,
     pub privkey_path: String,
-    pub host: [u8; 4],
-    pub port: u16
+    pub listen: SocketAddr,
+    pub starting_id: u32
 }
 
 impl Default for ProxyConfig {
@@ -44,127 +46,146 @@ impl Default for ProxyConfig {
         Self {
             pubkey_path: "data/cert".to_string(),
             privkey_path: "data/key".to_string(),
-            host: [0, 0, 0, 0],
-            port: 1337
+            listen: SocketAddr::from((
+                [0, 0, 0, 0],
+                1337
+            )),
+            starting_id: 0
         }
     }
 }
 
 impl ProxyConfig {
-    pub fn build(self) -> Arc<Proxy>{
-        Proxy::new(self)
+    pub fn build(self) -> ProxyServer {
+        ProxyServer::new(self)
     }
 }
 
-pub struct Proxy {
-    host: [u8; 4],
-    port: u16, 
-    cert_store: Arc<CertStore>,
+pub struct ProxyServer {
+    listen: SocketAddr,
     request_sink: Receiver<ProxyEvent>,
-    request_source: Sender<ProxyEvent>,
-    id: Arc<AtomicU32>,
+    core: ProxyCore,
 }
 
-impl Proxy {
-    fn new(config: ProxyConfig) -> Arc<Self> {
-        let(tx, rx) = channel(16);
-        Arc::new(Self {
-            host: config.host,
-            port: config.port,
-            cert_store: Arc::new(
-                CertStore::load_or_create(&config.pubkey_path, &config.privkey_path)
-            ),
-            request_source: tx,
+impl ProxyServer {
+    pub fn new(conf: ProxyConfig) -> Self {
+        let (tx, rx) = channel(64);
+        Self {
+            listen: conf.listen,
             request_sink: rx,
-            id: Arc::new(AtomicU32::new(0))
-        })
-    }
-
-    pub fn run(proxy: Arc<Self>) -> JoinHandle<Result<(), hyper::Error>>{
-        let proxy_move = proxy.clone();
-        let make_svc = make_service_fn(move |_socket: &AddrStream| {
-            let proxy = proxy_move.clone();
-            async move {
-                Ok::<_, Infallible>(service_fn(move |req| {
-                    let proxy = proxy.clone();
-                    Self::handle_with_upgrade(proxy, req)
-                }))
+            core: ProxyCore {
+                cert_store: Arc::new(CertStore::load_or_create(&conf.pubkey_path, &conf.privkey_path)),
+                request_source: Arc::new(tx),
+                id: Arc::new(AtomicU32::new(conf.starting_id)),
+                fallback_host: None,
+                client: Client::builder().build(hyper_tls::HttpsConnector::new())
             }
-        });
-        let addr = SocketAddr::from((proxy.host.to_owned(), proxy.port));
-
-        tokio::spawn(Server::bind(&addr).serve(make_svc))
+        }
     }
 
-    async fn wrap_tls(proxy: Arc<Self>, upgraded: Upgraded, host: &Uri) -> Result<(), Error> {
-        let proxy = proxy.clone();
-        let acceptor = TlsAcceptor::from(
-            Arc::new(
-                rustls::ServerConfig::builder()
-                    .with_safe_default_cipher_suites()
-                    .with_safe_default_kx_groups()
-                    .with_safe_default_protocol_versions()
-                    .unwrap()
-                    .with_no_client_auth()
-                    .with_cert_resolver(CertStore::build_cert(
-                        &proxy.cert_store,
-                        host.host().map(|host| host.to_owned()),
-                    )),
-            )
-        );
-        let stream = acceptor.accept(upgraded).await.unwrap();
-        let host = String::from(
-            stream
-                .get_ref()
-                .1
-                .sni_hostname()
-                .unwrap_or(host.host().unwrap()),
-        );
-        if let Err(e) = Http::new()
-            .serve_connection(
-                stream,
-                service_fn(move |mut req| {
+    pub fn run(self) -> JoinHandle<Result<(), hyper::Error>>{
+        tokio::spawn (
+            Server::bind(&self.listen)
+            .serve(self)
+        )
+    }
+}
+
+impl<'a> Service<&'a AddrStream> for ProxyServer {
+    type Response = ProxyCore;
+
+    type Error = Infallible;
+
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+
+    fn poll_ready(&mut self, _cx: &mut std::task::Context<'_>) -> std::task::Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, _req: &'a AddrStream) -> Self::Future {
+        let core = self.core.clone();
+        let fut = async move {
+            Ok(core)
+        };
+        Box::pin(fut)
+    }
+}
+
+#[derive(Clone)]
+pub struct ProxyCore {
+    cert_store: Arc<CertStore>,
+    request_source: Arc<Sender<ProxyEvent>>,
+    id: Arc<AtomicU32>,
+    fallback_host: Option<String>,
+    client: Client<hyper_tls::HttpsConnector<hyper::client::HttpConnector>, Body>
+}
+
+impl Service<Request<Body>> for ProxyCore {
+    type Response = Response<Body>;
+
+    type Error = Error;
+
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+
+    fn poll_ready(&mut self, _cx: &mut std::task::Context<'_>) -> std::task::Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, mut req: Request<Body>) -> Self::Future {
+        let proxy = self.clone();
+        let host = req.uri().host().map(String::from);
+        Box::pin(async move {
+            if let &Method::CONNECT = req.method()
+            {
+                tokio::spawn(async move {
+                    match upgrade::on(req).await {
+                        Ok(upgraded) => {
+                            proxy.do_tls_upgrade(upgraded, host).await;
+                        },
+                        Err(e) => {
+                            eprint!("Error upgrading connection: {}", e);
+                        }
+                    }
+                });
+                Ok(Response::default())
+            }
+            else {
+                if let Some(host) = host.or(proxy.fallback_host){
+                    let id = proxy.id.fetch_add(1, Ordering::Relaxed);
                     let mut uri = req.uri().to_owned().into_parts();
                     uri.authority = Some(Authority::from_maybe_shared(host.clone()).unwrap());
                     uri.scheme = Some(Scheme::HTTPS);
                     *req.uri_mut() = Uri::from_parts(uri).unwrap();
-                    Self::handle_req(req, proxy.request_source.clone(), proxy.id.clone())
-                }),
-            )
-            .await
-        {
-            eprintln!("TLS Proxy Error: {}", e)
-        }
-        Ok(())
-    }
-
-    async fn handle_with_upgrade(proxy: Arc<Self>, mut req: Request<Body>) -> Result<Response<Body>, Infallible> {
-        match req.method() {
-            &Method::CONNECT => {
-                tokio::spawn(async move {
-                    let proxy = proxy.clone();
-                    match hyper::upgrade::on(&mut req).await {
-                        Ok(upgraded) => {
-                            if let Err(e) = Self::wrap_tls(proxy, upgraded, req.uri()).await {
-                                eprintln!("TLS proxy error: {}", e)
-                            }
-                        }
-                        Err(e) => eprintln!("CONNECT Error: {}", e),
-                    }
-                });
-                let resp = Response::new(Body::empty()); // Empty response, CONNECT expects a 200
-                Ok(resp)
+                    println!("Request: {} {:?}", id, req);
+                    Ok(proxy.client.request(req).await.unwrap())
+                } else {
+                    Err(Error::new(ErrorKind::InvalidInput ,"No host requested and no host derived from CONNECT or SNI"))
+                }
             }
-            _default => Self::handle_req(req, proxy.request_source.clone(), proxy.id.clone()).await,
-        }
+        })
+    }
+}
+
+impl ProxyCore {
+    async fn do_tls_upgrade(&self, conn: Upgraded, fallback_host: Option<String>) {
+        let conf = Arc::new(ServerConfig::builder()
+            .with_safe_default_cipher_suites()
+            .with_safe_default_kx_groups()
+            .with_safe_default_protocol_versions()
+            .unwrap()
+            .with_no_client_auth()
+            .with_cert_resolver(CertStore::build_cert(&self.cert_store, fallback_host.to_owned())));
+        let accepted = TlsAcceptor::from(conf).accept(conn).await.unwrap();
+        println!("TLS connected with SNI {:?}", accepted.get_ref().1.sni_hostname());
+        let mut service = self.clone();
+        service.fallback_host = Self::get_host(&accepted, &fallback_host);
+        if let Err(e) = Http::new().serve_connection(accepted, service).await
+            {
+                eprintln!("Error serving wrapped connection: {}", e)
+            }
     }
 
-    async fn handle_req(req: Request<Body>, channel: Sender<ProxyEvent>, id: Arc<AtomicU32>) -> Result<Response<Body>, Infallible> {
-        let id = id.fetch_add(1, Ordering::Relaxed);
-        println!("{} Got request {:?}", id, req);
-        let client = Client::builder().build(hyper_tls::HttpsConnector::new());
-        let resp = client.request(req).await.unwrap();
-        println!("{} Got response {:?}", id, resp);
-        Ok(resp)
+    fn get_host(conn: &TlsStream<Upgraded>, fallback_host: &Option<String>) -> Option<String> {
+        conn.get_ref().1.sni_hostname().map(String::from).or(fallback_host.to_owned())
     }
 }
